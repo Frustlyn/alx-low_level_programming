56516525727cd0f3f0ea4ad84c8c464d6a64deff
Frustlyn <chidozieossai01@gmail.com>
2022-09-12 07:04:39 -0700

    The program that prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 'a'; num > 'z'; num++)
+		putchar(num);
+	return (0);
+}
