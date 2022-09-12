94db79fcbb1133ceb581f4784a379c72104ea7c4
Frustlyn <chidozieossai01@gmail.com>
2022-09-12 07:30:08 -0700

    Next level

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-        char num1;
+
+	char num1;
 	char num2;
 
-        for (num1 = 'a'; num1 <= 'z'; num1++)
-		for (num2 = 'A' num2 <='Z' num2++)
-			putchar(num1);
-                putchar(num2);
-        putchar('\n');
-        return (0);
+	for (num1 = 'a'; num1 <= 'z'; num1++)
+	putchar(num1);
+	for (num2 = 'A'; num2 <= 'Z'; num2++)
+	putchar(num1);
+	putchar(num2);
+	putchar('\n');
+	return (0);
 }
